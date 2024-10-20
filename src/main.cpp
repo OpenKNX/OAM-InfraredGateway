593cdca804260295eaa519364890a8018a247aad
Marco Scholl <develop@marco-scholl.de>
2024-10-20 12:50:24 +0200

    refactoring to new ofm-infrared

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
-#include <Arduino.h>
-#include "OpenKNX.h"
-#include "IrCodeModule.h"
 #include "FileTransferModule.h"
+#include "InfraredModule.h"
+#include "OpenKNX.h"
+#include "VirtualButtonModule.h"
+#include "Logic.h"
 
 void setup()
 {
-	const uint8_t firmwareRevision = 0;
+    const uint8_t firmwareRevision = 0;
     openknx.init(firmwareRevision);
-    openknx.addModule(1, openknxIrCodeModule);
-    openknx.addModule(2, openknxFileTransferModule);
+    openknx.addModule(1, openknxLogic);
+    openknx.addModule(3, openknxVirtualButtonModule);
+    openknx.addModule(4, openknxInfraredModule);
+    openknx.addModule(9, openknxFileTransferModule);
     openknx.setup();
 }
 
